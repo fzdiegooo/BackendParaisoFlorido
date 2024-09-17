ac2f2bdb04650a89c7dd043c99ee2ea8d45aa400
Diego <zxiteft21@gmail.com>
2024-09-17 03:00:12 -0500

    Fix Register Controller

--- src/controllers/asistencia.controller.js
+++ src/controllers/asistencia.controller.js
@@ -1,6 +1,7 @@
 import { Usuario } from "../models/Usuario.js";
 import { Asistencias } from "../models/Asistencias.js";
 import moment from "moment/moment.js";
+import 'moment/locale/es.js';
 
 
 
@@ -33,24 +34,29 @@ export const registro = async (req, res) => {
             where: {usuarioId: id, fecha: fechaActual}
         });
 
-        if (asistencia ) {
-            if(new Date() - new Date(`${fechaActual}T${asistencia.ingreso}`) >= 15 * 60 * 1000){
+        if (asistencia) {
+            // Calcular la diferencia en minutos desde el ingreso hasta el momento actual
+            const diferenciaMinutos = moment().diff(moment(`${fechaActual} ${asistencia.ingreso}`), 'minutes');
+
+            // Verificar si han pasado más de 15 minutos desde el registro de ingreso
+            if (diferenciaMinutos >= 15) {
+                // Actualizar el registro de salida
                 await asistencia.update({
-                    salida: new Date().toLocaleTimeString("es-PE")
+                    salida: horaActual
                 });
                 return res.status(200).json({ message: "Registro de salida exitoso" });
-            }else{
-                return res.status(400).json({ message: "No se puede registrar una asistencia si almenos no pasan 15 min" });
+            } else {
+                return res.status(422).json({ message: "Deben pasar al menos 15 minutos para registrar la salida" });
             }
-            
         } else {
+            // Crear nuevo registro de ingreso
             await Asistencias.create({
                 fecha: fechaActual,
-                ingreso: new Date().toLocaleTimeString("es-PE"),
+                ingreso: horaActual,
                 salida: null,
                 usuarioId: id
             });
-            return res.status(200).json({ message: "Registro de ingreso exitoso" });
+            return res.status(201).json({ message: "Registro de ingreso exitoso" });
         }
 
     } catch (error) {
